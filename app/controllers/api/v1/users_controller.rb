89a9e7977da100021788925fd90d53f0b02f277b
Antonio Jefferson <antoniojeferson96@gmail.com>
2023-09-26 13:05:52 -0300

    feat: creating CRUD for users

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,51 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :set_user, except: %i[create index]
+
+      def index
+        @users = User.all
+      end
+
+      def show; end
+
+      def create
+        @user = User.new(user_params)
+
+        if @user.save
+          render status: :created
+        else
+          render json: { errors: @user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @user.update(user_params)
+          @user
+        else
+          render json: { errors: @user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @user if @user.destroy
+      end
+
+      private
+
+      def set_user
+        @user = User.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: {
+          errors: I18n.t('activerecord.errors.models.user.not_found')
+        }, status: :not_found
+      end
+
+      def user_params
+        params.permit(
+          :name, :email, :password_digest
+        )
+      end
+    end
+  end
+end
