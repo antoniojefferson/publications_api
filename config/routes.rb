426b1780d98418b1f967d593125087072b62c5f5
Antonio Jefferson <antoniojeferson96@gmail.com>
2023-09-26 19:15:43 -0300

    feat: creating the controller with its route for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :authentication do
+        post :sign_in, defaults: { format: :json }
+      end
+
       resources :users, defaults: { format: :json }
     end
   end
