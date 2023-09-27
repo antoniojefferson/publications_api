8bd3737b53857a53adf4b0b357bcb8342687f61d
Antonio Jefferson <antoniojeferson96@gmail.com>
2023-09-27 10:45:11 -0300

    fix: correcting the validation structure and password definition

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   validates :name, presence: true
-  validates :password_digest, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
