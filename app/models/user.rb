f7ae80815d44e5507b8baabcf42d248d09033e6a
Antonio Jefferson <antoniojeferson96@gmail.com>
2023-09-26 23:22:26 -0300

    refactor: adding has_many relationship with posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :posts, dependent: :destroy
+
   validates :name, presence: true
   validates :password_digest, presence: true, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true
